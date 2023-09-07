73cf94bb31f45f086a12d77de74ef327b11a9547
Zakariya Al-Khamisi <17160061@su.edu.ye>
2023-09-08 01:25:54 +0300

    Update api.py

--- command-line-shell/after/src/shell/api.py
+++ command-line-shell/after/src/shell/api.py
@@ -3,26 +3,20 @@ from .algorithms import DECODING_ALGORITHMS, ENCODING_ALGORITHMS, HASHING_ALGORI
 def encoding_algos() -> list[str]:
     return list(ENCODING_ALGORITHMS.keys())
 
-
 def decoding_algos() -> list[str]:
     return list(DECODING_ALGORITHMS.keys())
-
 
 def hashing_algos() -> list[str]:
     return list(HASHING_ALGORITHMS.keys())
 
-
 def has_encoding_algo(algo: str) -> bool:
     return algo.lower().strip() in ENCODING_ALGORITHMS
-
 
 def has_decoding_algo(algo: str) -> bool:
     return algo.lower().strip() in DECODING_ALGORITHMS
 
-
 def has_hashing_algo(algo: str) -> bool:
     return algo.lower().strip() in HASHING_ALGORITHMS
-
 
 def encode(text: str | bytes, algo: str) -> str:
     if isinstance(text, str):
@@ -30,13 +24,11 @@ def encode(text: str | bytes, algo: str) -> str:
     encoding_fn = ENCODING_ALGORITHMS[algo.lower().strip()]
     return encoding_fn(text).decode()
 
-
 def decode(text: str | bytes, algo: str) -> str:
     if isinstance(text, str):
         text = text.encode()
     decoding_fn = DECODING_ALGORITHMS[algo.lower().strip()]
     return decoding_fn(text).decode()
-
 
 def hash_val(text: str | bytes, algo: str) -> str:
     if isinstance(text, str):
